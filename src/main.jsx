246a2d49bfba710b43bba6799b3fdef0396c002d
Harshit Garg <harshit118garg@gmail.com>
2023-03-05 20:13:28 +0530

    theme updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/react-toastify/dist/ReactToastify.min.css";
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
